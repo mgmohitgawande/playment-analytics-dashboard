c10b3aa384e16dc9063e56b021c9c46fe8005545
Mohit Gawande <mgmohitgawande@gmail.com>
2016-01-03 13:40:48 +0530

    modification suggested
    zczsaefa

--- public/js/dashboard1.js
+++ public/js/dashboard1.js
@@ -20,46 +20,15 @@ function makeGraphs(error, usersData, tasksData, task_submissionData) {
     tasksData.forEach(function(d){
         d.gender=genderHash[d.user_id];
         if(d.gender==undefined)   console.log(d);
-        d.created_at=d.created_at.substring(0, d.created_at.length-3)+d.created_at.substring(d.created_at.length-2, d.created_at.length)
         
-        var abc = dateFrmt.parse(d.created_at);
-        if(abc){
-            d.created_at=abc;
-        }
-        else{
-            var dateFrmt1 = d3.time.format("%Y-%m-%d %H:%M:%S%Z");
-            abc = dateFrmt1.parse(d.created_at);
-        }
-        
-        if(abc){
-            d.created_at=abc;
-        }
-        else{
-            console.log(d.created_at);
-        }
+        d.created_at = new Date(d.created_at);
     });
     
     //for task_submissions
     task_submissionData.forEach(function(d){
         d.gender=genderHash[d.user_id];
-        d.created_at=d.created_at.substring(0, d.created_at.length-3)+d.created_at.substring(d.created_at.length-2, d.created_at.length)
         
-        var abc = dateFrmt.parse(d.created_at);
-        if(abc){
-            d.created_at=abc;
-        }
-        else{
-            console.log(d.created_at);
-            var dateFrmt1 = d3.time.format("%Y-%m-%d %H:%M:%S%Z");
-            abc = dateFrmt1.parse(d.created_at);
-        }
-        
-        if(abc){
-            d.created_at=abc;
-        }
-        else{
-            console.log(d.created_at);
-        }
+        d.created_at = new Date(d.created_at);
     })
     
     //crossfilter instance for users
@@ -87,7 +56,7 @@ function makeGraphs(error, usersData, tasksData, task_submissionData) {
     var tasks = crossfilter(tasksData);
     
     //tasks => 1. define created_at dimension, 2. group tasks based on gender
-    var taskCreated = tasks.dimension(function(d){ return d.created_at;});
+    var taskCreated = tasks.dimension(function(d){ return d3.time.hour(d.created_at);});
     var taskByCreatedDate = taskCreated.group();
     //tasks picked vs time line chart
     var taskDateChart = dc.lineChart("#date-task-chart");
@@ -134,7 +103,7 @@ function makeGraphs(error, usersData, tasksData, task_submissionData) {
     var submissions = crossfilter(task_submissionData);
     
     //tasks-submission => 1. define created_at dimension, 2. group tasks based on gender
-    var taskSubmitted = submissions.dimension(function(d){ return d.created_at;});
+    var taskSubmitted = submissions.dimension(function(d){ return d3.time.hour(d.created_at);});
     var taskBySubmittedDate = taskSubmitted.group();
     
     //tasks-submission vs time line chart
